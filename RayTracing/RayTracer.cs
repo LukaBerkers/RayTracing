f6c3f9de27b06d4526cef8d32bfd4138807c5121
Luka <luukberkers@protonmail.com>
2023-05-25 16:42:22 +0200

    Unfinished code for drawing circles

--- RayTracing/RayTracer.cs
+++ RayTracing/RayTracer.cs
@@ -26,6 +26,8 @@ public class RayTracer
         throw new NotImplementedException();
     }
 
+    #region Debug
+
     public void Debug(float height = 0.0f)
     {
         Display.Clear(0);
@@ -41,13 +43,23 @@ public class RayTracer
         var rightVec = (screen.TopRight + screen.BottomRight) / 2;
         var left = DebugWorldToScreen(leftVec);
         var right = DebugWorldToScreen(rightVec);
+        // Draw a line between them
         Display.Line(left.X, left.Y, right.X, right.Y, 0xff_ff_ff);
+
+        // Draw the spheres
+        foreach (var sphere in _scene.Primitives.OfType<Sphere>())
+        {
+            var circleRadius = DebugSpherePlaneIntersectionRadius(sphere, height);
+            if (float.IsNaN(circleRadius))
+                break;
+            DebugDrawCircle(sphere.Position.Xz, circleRadius);
+        }
     }
 
     private Vector2i DebugWorldToScreen(Vector3 vec)
     {
         // First flatten; assume the vector is in the right y plane
-        Vector2 flat = (vec.X, vec.Z);
+        var flat = vec.Xz;
 
         // Amount of pixels per unit lenght
         const int scale = 32;
@@ -55,10 +67,37 @@ public class RayTracer
         Vector2i originInScreenSpace = (Display.Width / 2, Display.Height * 7 / 8);
 
         // Then convert to integer representation
-        var pixel = originInScreenSpace + (Convert.ToInt32(flat.X * scale), Convert.ToInt32(flat.Y * scale));
-
+        var pixel = originInScreenSpace + (Vector2i)(flat * scale);
+        
         return pixel;
     }
+
+    /// <summary>
+    ///     Find the radius of the circle that forms the intersection of a sphere with a certain y-plane.
+    /// </summary>
+    /// <param name="sphere">The sphere to calculate the intersection for.</param>
+    /// <param name="height">The y-value of the intersection plane.</param>
+    /// <returns>
+    ///     The radius when there is an intersection.
+    ///     NaN if there is no intersection.
+    /// </returns>
+    private static float DebugSpherePlaneIntersectionRadius(Sphere sphere, float height)
+    {
+        // The radius we are looking for is:
+        //  sqrt( r^2 - (a - k)^2 )
+        // where: r is the radius of the sphere
+        //        a is the plane height value
+        //        k is the y-value of the sphere center
+
+        return float.Sqrt(sphere.Radius * sphere.Radius - (height - sphere.Position.Y) * (height - sphere.Position.Y));
+    }
+
+    private void DebugDrawCircle(Vector2 center, float radius)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
 }
 
 public struct Ray
