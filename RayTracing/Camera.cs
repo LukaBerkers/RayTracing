5dce3b820c464faa60aa5de33a2666ba5a01d5f5
Luka <luukberkers@protonmail.com>
2023-05-26 15:39:26 +0200

    Make sure the up, lookAt and right vectors are always unit and form a right-handed basis

--- RayTracing/Camera.cs
+++ RayTracing/Camera.cs
@@ -4,27 +4,47 @@ namespace RayTracing;
 
 public class Camera
 {
+    private Vector3 _lookAt;
+    private Vector3 _right;
+    private Vector3 _up;
     public Vector3 Position;
-    public Vector3 LookAt;
-    public Vector3 Up;
     public ScreenPlane ScreenPlane;
 
     public Camera(Vector3 position, Vector3 lookAt, Vector3 up)
     {
         Position = position;
-        LookAt = lookAt;
-        Up = up;
+        _lookAt = lookAt.Normalized();
+        _up = up.Normalized();
         var midScreen = position + lookAt;
-        var right = Vector3.Cross(lookAt, up);
+        _right = Vector3.Cross(lookAt, up);
         ScreenPlane = new ScreenPlane
         {
-            TopLeft = midScreen + up - right,
-            TopRight = midScreen + up + right,
-            BottomLeft = midScreen - up - right,
-            BottomRight = midScreen - up + right
+            TopLeft = midScreen + up - _right,
+            TopRight = midScreen + up + _right,
+            BottomLeft = midScreen - up - _right,
+            BottomRight = midScreen - up + _right
         };
     }
 
+    public Vector3 LookAt
+    {
+        get => _lookAt;
+        set
+        {
+            _lookAt = value.Normalized();
+            _right = Vector3.Cross(value, Up);
+        }
+    }
+
+    public Vector3 Up
+    {
+        get => _up;
+        set
+        {
+            _up = value.Normalized();
+            _right = Vector3.Cross(_lookAt, value);
+        }
+    }
 }
 
 public struct ScreenPlane
@@ -35,4 +55,3 @@ public struct ScreenPlane
     public Vector3 BottomLeft; // (-1, -1, -1)
     public Vector3 BottomRight; // (+1, -1, -1)
 }
-
