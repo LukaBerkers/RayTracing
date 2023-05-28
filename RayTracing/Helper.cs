ebc367a4505b3d9127a5132c3ffe339765b0cd3b
Luka <luukberkers@protonmail.com>
2023-05-28 13:50:05 +0200

    Float compare function

--- RayTracing/Helper.cs
+++ RayTracing/Helper.cs
@@ -6,9 +6,10 @@ public static class Helper
     {
         return value == 0.0f || float.IsSubnormal(value);
     }
-
+    
     public static int Compare(float left, float right)
     {
-        throw new NotImplementedException();
+        // This will probably need a bigger error margin in the future, for now this works.
+        return IsZero(left - right) ? 0 : left.CompareTo(right);
     }
 }
