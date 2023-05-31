adace666cef4c02ffd41c9adbd524fa6c11efb00
Luka <luukberkers@protonmail.com>
2023-05-31 22:50:38 +0200

    Draw planes in debug output

--- RayTracing/MyApplication.cs
+++ RayTracing/MyApplication.cs
@@ -13,13 +13,13 @@ public class MyApplication
         var lights = new List<Light> { new((3, 4, 0), (8, 24, 24)), new((-1, 5, -1), (24, 8, 8)) };
 
         var plane = new Plane(Vector3.UnitY, -1.0f, (0.5f, 0.125f, 0.5f));
-        // var mirrorWall = new Plane((1.0f, 0.0f, 1.0f), -10.0f, Vector3.Zero, Primitive.MaterialType.Mirror);
+        var mirrorWall = new Plane((1.0f, 0.0f, 1.0f), -10.0f, Vector3.One, Primitive.MaterialType.Mirror);
         Vector3 copperColor = (13.0f / 18.0f, 9.0f / 20.0f, 0.2f);
         var copperBall = new Sphere((-2, 0, -4), 1, copperColor, Primitive.MaterialType.Metal);
         var greenSphere = new Sphere((2, 1, -6), 4, (0, 0.5f, 0), Primitive.MaterialType.Plastic);
         var mirrorBall = new Sphere((-2, 2, -6), 2, Vector3.One, Primitive.MaterialType.Mirror);
 
-        var shapes = new List<Primitive> { plane, copperBall, greenSphere, mirrorBall };
+        var shapes = new List<Primitive> { plane, copperBall, greenSphere, mirrorBall, mirrorWall };
 
         _rayTracer = new RayTracer(screen, lights, shapes);
     }
